8166a15b4201cf8bef55f1a437cdadf465382c9b
Phi Long Nguyen <phi-long.nguyen@ticos-systems.ch>
2019-06-21 13:45:24 +0200

    Add all files

---
+++ WebUI/Models/Games/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebUI.Models.Games
+{
+    public class Event
+    {
+        public int GameId { get; set; }
+        public int EventTypeId { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Source { get; set; }
+    }
+}
